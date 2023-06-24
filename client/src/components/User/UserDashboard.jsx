0d80ffc112a7a2909b85ac8734ad06abdb66b0a6
dganesh22 <dganesh2022@gmail.com>
2023-06-24 16:15:53 +0530

    project completed

--- client/src/components/User/UserDashboard.jsx
+++ client/src/components/User/UserDashboard.jsx
@@ -27,8 +27,47 @@ function UserDashboard() {
   },[])
 
   return (
-    <div>
-      User dashboard
+    <div className='container-fluid'>
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <h3 className="display-3 text-success">User Dashboard</h3>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-12">
+              <table className="table table-bordered table-striped table-hovered">
+                 <thead>
+                    <tr className='text-center'>
+                        <th>Book Title</th>
+                        <th>Image</th>
+                        <th>Amount</th>
+                        <th>Rented Date</th>
+                        <th>Return Date</th>
+                      </tr>
+                 </thead>
+                  <tbody>
+                      {
+                          rent && rent.map((item,index) => {
+                            const { book, user, amount, rentDate, returnDate } = item
+
+                            return (
+                              <tr className="text-center" key={index}>
+                                  <td> { book.title ? book.title : ''} </td>
+                                  <td>
+                                      { book.image?  (<img src={ book.image.url } alt='no image found' width={100} height={80} />): ''}  
+                                  </td>
+                                  <td> &#8377; { amount } </td>
+                                  <td> { new Date(rentDate).toLocaleString() } </td>
+                                  <td> { new Date(returnDate).toLocaleString() }  </td>
+                              </tr>
+                            )
+                          })
+                      }
+                  </tbody>
+              </table>
+          </div>
+        </div>
     </div>
   )
 }
